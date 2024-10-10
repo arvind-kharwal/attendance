5b3e13b6f6085f2cc2e1c936c91da2fff9731f4a
Arvind Kharwal <karvind629@gmail.com>
2024-10-10 15:59:50 +0530

    First Commit

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import pandas as pd
 st.title('Hello')
 st.header("Header")
 st.header("Dataframe Example")
-data = pd.read_excel("details.xlsx")
+data = pd.read_csv("details.xlsx",encoding='ISO-8859-1')
 st.dataframe(data)
