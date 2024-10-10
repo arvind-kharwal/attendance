abd39dad8836041241a4855a8884ff0d47d33d1f
Arvind Kharwal <karvind629@gmail.com>
2024-10-10 16:11:22 +0530

    Excel

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 st.title('CSE17 Attendance')
 st.header("Dataframe Example")
-data = pd.read_excel("details.xlsx")
+data = pd.read_excel("sheet.xlsx")
 st.dataframe(data)
